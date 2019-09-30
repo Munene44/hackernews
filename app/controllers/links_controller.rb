c0f44ee95a1d3dca0fd7b95c1a0680585d4d4486
nenekelvin <karvinmitchar@gmail.com>
2019-09-30 13:12:37 +0300

    downvote method added

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -25,7 +25,11 @@ before_action :authenticate_user!, except: [:index, :show]  # GET /links
     @link.upvote_by current_user
     redirect_back fallback_location: root_path
   end
-
+ def downvote
+    @link = Link.find(params[:id])
+    @link.downvote_by current_user
+    redirect_back fallback_location: root_path
+  end
   # POST /links
   # POST /links.json
   def create
